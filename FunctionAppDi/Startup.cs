8d91404733d5049d05e3f1911d3db550da33667f
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-22 21:28:34 +0200

    Azure Function DI
    Added azure function project and created DI setup with sample function

---
+++ FunctionAppDi/Startup.cs
@@ -0,0 +1,20 @@
+﻿using DIDemoLib;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(FunctionAppDi.Startup))]
+namespace FunctionAppDi
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<IMesseges, Messeges>();
+        }
+    }
+}
