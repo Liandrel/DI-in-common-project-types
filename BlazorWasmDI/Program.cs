80ee5d7b3d4ba9ab2175ed40090bfcda9a177edb
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-22 21:14:00 +0200

    BlazorWasm Di
    added sample Blazor Wasm DI call in index page

---
+++ BlazorWasmDI/Program.cs
@@ -0,0 +1,13 @@
+using BlazorWasmDI;
+using DIDemoLib;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<IMesseges, Messeges>();
+
+await builder.Build().RunAsync();
