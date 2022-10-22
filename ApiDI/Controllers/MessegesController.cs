376c84b285e1bf65ffecb35996ce765df19a5813
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-22 21:08:56 +0200

    ASP.NET Core
    added api project with sample controller using DI

---
+++ ApiDI/Controllers/MessegesController.cs
@@ -0,0 +1,25 @@
+﻿using DIDemoLib;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiDI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessegesController : ControllerBase
+    {
+        private readonly IMesseges _messeges;
+
+        public MessegesController(IMesseges messeges)
+        {
+            _messeges = messeges;
+        }
+        // GET: api/<MessegesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { _messeges.SayHello(), _messeges.SayGoodBye()};
+        }
+    }
+}
